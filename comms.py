3353a22edc865147e9c73977394afddbbc950bfd
samurai444412 <eduardmirceabraguta13.1.2004@gmail.com>
2025-11-22 22:57:39 +0200

    Describe what you changed in orchestrator

--- comms.py
+++ comms.py
@@ -6,6 +6,8 @@ SERVICE_URLS = {
     "orchestrator": "http://127.0.0.1:5001",
     "legal": "http://127.0.0.1:5002",
     "predictor": "http://127.0.0.1:5003",
+    "financial": "http://127.0.0.1:5004",
+    "frontend": "http://127.0.0.1:5005",
 }
 
 
